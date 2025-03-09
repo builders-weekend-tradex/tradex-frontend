66052224c7fff281b5cb70f90e6fe1c8f5a790db
Michael Whipps <michaeljwhipps@gmail.com>
2025-03-09 21:25:02 +0900

    fix: use enums for the active tab state.

--- src/components/pages/AnalyticsPage.tsx
+++ src/components/pages/AnalyticsPage.tsx
@@ -3,49 +3,53 @@ import FundamentalsTab from "../analytics/fundamentals/FundamentalsTab";
 import TechnicalsTab from "../analytics/technicals/TechnicalsTab";
 import { useState } from "react";
 
+enum Tab {
+  Social = "social",
+  Fundamentals = "fundamentals",
+  Technicals = "technicals",
+}
+
 const AnalyticsPage: React.FC = () => {
-  const [activeTab, setActiveTab] = useState<
-    "social" | "fundamentals" | "technicals"
-  >("social");
+  const [activeTab, setActiveTab] = useState<Tab>(Tab.Social);
 
   return (
     <div className="w-full">
       <div className="flex space-x-1">
         <button
           className={`px-4 py-2 hover:bg-gray-100 rounded-t-lg focus:outline-none ${
-            activeTab === "social"
+            activeTab === Tab.Social
               ? "bg-white text-gray-700"
               : "bg-gray-200 text-gray-700"
           }`}
-          onClick={() => setActiveTab("social")}
+          onClick={() => setActiveTab(Tab.Social)}
         >
           Social
         </button>
         <button
           className={`px-4 py-2 hover:bg-gray-100 rounded-t-lg focus:outline-none ${
-            activeTab === "fundamentals"
+            activeTab === Tab.Fundamentals
               ? "bg-white text-gray-700"
               : "bg-gray-200 text-gray-700"
           }`}
-          onClick={() => setActiveTab("fundamentals")}
+          onClick={() => setActiveTab(Tab.Fundamentals)}
         >
           Fundamentals
         </button>
         <button
           className={`px-4 py-2 hover:bg-gray-100 rounded-t-lg focus:outline-none ${
-            activeTab === "technicals"
+            activeTab === Tab.Technicals
               ? "bg-white text-gray-700"
               : "bg-gray-200 text-gray-700"
           }`}
-          onClick={() => setActiveTab("technicals")}
+          onClick={() => setActiveTab(Tab.Technicals)}
         >
           Technicals
         </button>
       </div>
       <div className="min-h-screen bg-white">
-        {activeTab === "social" && <SocialTab />}
-        {activeTab === "fundamentals" && <FundamentalsTab />}
-        {activeTab === "technicals" && <TechnicalsTab />}
+        {activeTab === Tab.Social && <SocialTab />}
+        {activeTab === Tab.Fundamentals && <FundamentalsTab />}
+        {activeTab === Tab.Technicals && <TechnicalsTab />}
       </div>
     </div>
   );
