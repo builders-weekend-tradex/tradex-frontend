e6a80245c4c338841b967d0e98fd01ffc66923ca
Michael Whipps <michaeljwhipps@gmail.com>
2025-03-23 00:02:46 +0900

    feat: add caching for news. Add days ago for publishedat date. Style.

--- src/components/analytics/social/SocialTab.tsx
+++ src/components/analytics/social/SocialTab.tsx
@@ -1,9 +1,11 @@
-// import NewsArticles from "./NewsArticles";
+import NewsArticles from "./NewsArticles";
 
 const SocialTab: React.FC = () => {
   return (
     // Commented out news articles component to save api calls
-    <div className="bg-white flex-1 min-h-full">{/* <NewsArticles /> */}</div>
+    <div className="bg-white flex-1 min-h-full">
+      <NewsArticles />
+    </div>
   );
 };
 
