756e238b8c8cdf9b5cd58e3086817fc99fcc7649
NabbeunNabi <williamedwardbrammer@gmail.com>
2025-03-24 23:20:55 +0900

    fix: vercel.json

--- react-router.config.ts
+++
@@ -1,7 +0,0 @@
-import { vercelPreset } from '@vercel/react-router/vite';
-import type { Config } from '@react-router/dev/config';
- 
-export default {
-  ssr: false,
-  presets: [vercelPreset()],
-} satisfies Config;
